619a6af2390d117e7215e656e4f3e00137681b2b
Payton Swick <payton@foolord.com>
2015-04-17 15:29:21 -0400

    Remove extraneous PHP singleton code in favor of static methods

--- custom-fonts-typekit.php
+++ custom-fonts-typekit.php
@@ -31,59 +31,14 @@ Author URI: http://automattic.com/
  */
 
 class Jetpack_Fonts_Typekit {
-	/**
-	 * Holds the single instance of this object
-	 * @var null|object
-	 */
-	private static $instance;
-
-	/**
-	 * Retrieve the single instance of this class, creating if
-	 * not previously instantiated.
-	 * @return object Jetpack_Fonts instance
-	 */
-	public static function get_instance() {
-		if ( ! self::$instance ) {
-			self::$instance = new self();
-		}
-		return self::$instance;
+	public static function init() {
+		add_action( 'jetpack_fonts_register', array( get_called_class(), 'register_provider' ) );
 	}
 
-	/**
-	 * Fires when the plugin is activated
-	 * @return void
-	 */
-	public static function on_activate() {
-		self::get_instance();
-	}
-
-	/**
-	 * Fires when the plugin is deactivated
-	 * @return void
-	 */
-	public static function on_deactivate() {
-		self::get_instance();
-	}
-
-	/**
-	 * Kicks things off on the init hook, loading what's needed
-	 * @return void
-	 */
-	public function init() {
-		add_action( 'jetpack_fonts_register', array( $this, 'register_provider' ) );
-	}
-
-	function register_provider( $jetpack_fonts ) {
+	public static function register_provider( $jetpack_fonts ) {
 		$provider_dir = dirname( __FILE__ ) . '/';
 		$jetpack_fonts->register_provider( 'typekit', 'Jetpack_Typekit_Font_Provider', $provider_dir . 'typekit.php' );
 	}
-
-	/**
-	 * Silence is golden
-	 */
-	protected function __construct() {}
 }
 
-add_action( 'init', array( Jetpack_Fonts_Typekit::get_instance(), 'init' ) );
-register_activation_hook( __FILE__, array( 'Jetpack_Fonts_Typekit', 'on_activate' ) );
-register_deactivation_hook( __FILE__, array( 'Jetpack_Fonts_Typekit', 'on_deactivate' ) );
+add_action( 'init', array( 'Jetpack_Fonts_Typekit', 'init' ) );
