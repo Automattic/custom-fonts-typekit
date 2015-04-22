7e6c385b78ab2eea98d54e7bc110e2c401c0c091
Payton Swick <payton@foolord.com>
2015-04-22 18:53:13 -0400

    Add fetching kit async with kit_id from options

--- typekit.php
+++ typekit.php
@@ -5,6 +5,10 @@ class Jetpack_Typekit_Font_Provider extends Jetpack_Font_Provider {
 
 	public $id = 'typekit';
 
+	private static $defaults = array(
+		'kit_id' => null
+	);
+
 	/**
 	 * Constructor
 	 * @param Jetpack_Fonts $custom_fonts Manager instance
@@ -921,40 +925,75 @@ class Jetpack_Typekit_Font_Provider extends Jetpack_Font_Provider {
 	 * @return void
 	 */
 	public function render_fonts( $fonts ) {
-		$typekit_domain = 'use.typekit.net';
-		$kit_id = ''; // TODO: get that
+		$fonts;
+		$kit_id = $this->get_kit_id();
 		if ( $kit_id ) {
-			echo '<script type="text/javascript" src="//' . $typekit_domain. '/' . $kit_id . '.js"></script>';
-			echo '<script type="text/javascript">try{Typekit.load();}catch(e){}</script>' . "\n";
+			$this->output_typekit_code( $kit_id );
 		}
 	}
 
+	private function output_typekit_code( $kit_id ) {
+		$config = array(
+			'kitId'         => esc_js( $kit_id ),
+			'scriptTimeout' => 3000,
+		);
+		$config = json_encode( $config );
+				echo
+<<<EMBED
+<script type="text/javascript" id="custom-fonts-js">
+(function(doc) {
+	var config = {$config},
+	h=doc.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+="wf-inactive"},config.scriptTimeout);var tk=doc.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=doc.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s);
+})(document);
+</script>
+
+EMBED;
+	}
+
+	private function get_kit_id() {
+		$this->get( 'kit_id' );
+	}
+
 	/**
-	 * Convert FVDs to an API string for variants.
-	 * @param  array $fvds FVDs
-	 * @return string      API variants
+	 * Retrieves the associative array where the plugin stores its data from the
+	 * WordPress option.
+	 *
+	 * @return array Returns an associative array of data for the plugin.
 	 */
-	private function fvds_to_api_string( $fvds ) {
-		$to_return = array();
-		foreach( $fvds as $fvd ) {
-			switch ( $fvd ) {
-				case 'n4':
-					$to_return[] = 'r'; break;
-				case 'i4':
-					$to_return[] = 'i'; break;
-				case 'n7':
-					$to_return[] = 'b'; break;
-				case 'i7':
-					$to_return[] = 'bi'; break;
-				default:
-					$style = substr( $fvd, 1, 1 ) . '00';
-					if ( 'i' === substr( $fvd, 0, 1 ) ) {
-						$style .= 'i';
-					}
-					$to_return[] = $style;
-			}
+	private function data() {
+		$data = get_option( 'typekit_data', self::$defaults );
+		$data = wp_parse_args( $data, self::$defaults );
+		return $data;
+	}
+
+	/**
+	 * Retrieves an individual plugin data value associated with the given key for
+	 * the current user.
+	 *
+	 * @param string $key The key of the plugin data value to get.
+	 * @return string Returns the current value for the given key.
+	 */
+	private function get( $key ) {
+		$data = $this->data();
+		return array_key_exists( $key, $data ) ? $data[$key] : null;
+	}
+
+	/**
+	 * Sets the plugin data value associated with the given key for the current
+	 * user.
+	 *
+	 * @param string $key The key of the plugin data value to set.
+	 * @param array|string|boolean|null $value The new value to be associated with this key.
+	 * @return boolean Returns true if the associated key was found and set, or false if the key isn't among the defaults.
+	 */
+	private function set( $key, $value ) {
+		$data = self::data();
+		if ( array_key_exists( $key, self::$defaults ) ) {
+			$data[$key] = $value;
+			update_option( 'typekit_data', $data );
+			return true;
 		}
-		return implode( ',', $to_return );
+		return false;
 	}
 
 	/**
@@ -997,6 +1036,7 @@ class Jetpack_Typekit_Font_Provider extends Jetpack_Font_Provider {
 	 * @return boolean|WP_Error true on success, WP_Error instance on failure.
 	 */
 	public function save_fonts( $fonts ) {
+		// TODO: save a new kit
 		return true;
 	}
 }
